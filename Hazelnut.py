584a68a8bf0a8b3eba1c6d9d40638bb64603c026
rcky225 <h.miura0225@gmail.com>
2024-02-12 19:13:58 +0900

    update model.h5

--- Hazelnut.py
+++ Hazelnut.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-model = load_model('./model.h5')#学習済みモデルをロード
+model = load_model('./model.h5', compile=False)#学習済みモデルをロード
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -37,7 +37,7 @@ def upload_file():
             filepath = os.path.join(UPLOAD_FOLDER, filename)
 
             #受け取った画像を読み込み、np形式に変換
-            img = image.load_img(filepath, grayscale=True, target_size=(image_size,image_size))
+            img = image.load_img(filepath, grayscale=False, target_size=(image_size,image_size,3))
             img = image.img_to_array(img)
             data = np.array([img])
             #変換したデータをモデルに渡して予測する
@@ -51,5 +51,4 @@ def upload_file():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host ='0.0.0.0',port = port)
+    app.run()
