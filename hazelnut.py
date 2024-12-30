7a5d8b9f46787a090d2e1236d72c076379efbe77
rcky225 <h.miura0225@gmail.com>
2024-12-30 20:04:53 +0900

    hazelnut.pyのインポートを整理し、画像処理のコードを更新しました。アップロードフォルダが存在しない場合は作成する機能を追加しました。

--- hazelnut.py
+++ hazelnut.py
@@ -2,12 +2,12 @@ import os
 import numpy as np
 from flask import Flask, request, redirect, render_template, flash
 from werkzeug.utils import secure_filename
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import load_img, img_to_array
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-classes = ["不良品","良品"]
+classes = ["不良品", "良品"]
 image_size = 100
 
 UPLOAD_FOLDER = "uploads"
@@ -15,11 +15,13 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-model = load_model('./model.h5', compile=False)#学習済みモデルをロード
-
+model = load_model('./model.h5', compile=False)  # 学習済みモデルをロード
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -36,19 +38,21 @@ def upload_file():
             file.save(os.path.join(UPLOAD_FOLDER, filename))
             filepath = os.path.join(UPLOAD_FOLDER, filename)
 
-            #受け取った画像を読み込み、np形式に変換
-            img = image.load_img(filepath, grayscale=False, target_size=(image_size,image_size,3))
-            img = image.img_to_array(img)
+            # 受け取った画像を読み込み、np形式に変換
+            img = load_img(filepath, target_size=(image_size, image_size, 3))
+            img = img_to_array(img)
             data = np.array([img])
-            #変換したデータをモデルに渡して予測する
+
+            # 変換したデータをモデルに渡して予測する
             result = model.predict(data)[0]
             predicted = result.argmax()
             pred_answer = "これは " + classes[predicted] + " です"
 
-            return render_template("index.html",answer=pred_answer)
+            return render_template("index.html", answer=pred_answer)
 
-    return render_template("index.html",answer="")
+    return render_template("index.html", answer="")
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
-    app.run(host ='0.0.0.0',port = port)
+    app.run(host='0.0.0.0', port=port)
+
